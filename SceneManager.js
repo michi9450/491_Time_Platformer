376397970f34293e87827a5d5f2a9c4f9955e75f
Alejandro A. Espino <michi9450.ae@gmail.com>
2026-01-26 15:32:37 -0800

    added framework for Scenemanager, will update as more objects are created

--- SceneManager.js
+++ SceneManager.js
@@ -9,6 +9,22 @@ class SceneManager {
     }
 
     clearEntities(){
+        this.game.entitiesPast.forEach(function (entity){
+            entitiy.removeFromWorld = true;
+        })
+        this.game.entitiesPresent.forEach(function (entity){
+            entitiy.removeFromWorld = true;
+        })
+    }
+    /*
+        level is which level the player is on
+        screen is which screen the player is on from the level (basically which part of the level)
+        x and y are the coordinates where the player should spawn if they die
+
+        in theory we will have a level.js for each level which will hold var for each screen which will be loaded by this method.
+    */
+    LoadScreen(level, screen, x, y){
         
     }
+
 }
